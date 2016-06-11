d72a69a6561e6c672c62258b3edbd35251c2fdba
witspirit <witspirit@gmail.com>
2016-06-11 16:34:30 +0200

    Did a simplistic attempt at the reflective test factory to see if that would be manageable... In my opinion no.

---
+++ src/main/java/be/witspirit/testfactory/support/ReflectiveTestFactory.java
@@ -0,0 +1,67 @@
+package be.witspirit.testfactory.support;
+
+import be.witspirit.testfactory.support.valueproviders.ValueProviders;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Tries to automatically populate a class based on reflection
+ */
+public class ReflectiveTestFactory<T> implements TestFactory<T> {
+    private Map<Class<?>, Supplier<?>> typeSuppliers = new HashMap<>();
+
+    private Class<T> targetType;
+
+    public ReflectiveTestFactory(Class<T> targetType) {
+        this.targetType = targetType;
+
+        setupDefaultSuppliers();
+    }
+
+    private void setupDefaultSuppliers() {
+        typeSuppliers.put(String.class, ValueProviders.randomString(10,20));
+        typeSuppliers.put(Integer.class, ValueProviders.intWithin(0,100));
+    }
+
+    public <V> ReflectiveTestFactory<T> provide(Class<V> type, Supplier<V> supplier) {
+        typeSuppliers.put(type, supplier);
+        return this;
+    }
+
+    @Override
+    public T create() {
+        try {
+            T obj = targetType.newInstance(); // Implies default constructor availability
+            for (Method method : targetType.getMethods()) {
+                if (method.getName().startsWith("set") && method.getParameterCount() == 1) {
+                    // Let us start with the simple case
+                    method.invoke(obj, getValue(method));
+                }
+            }
+
+            return obj;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Reflection problem", e);
+        }
+    }
+
+    private Object getValue(Method method) {
+        Class<?> parameterType = method.getParameterTypes()[0];
+
+        // Do we already know how to provide such a type ?
+        Supplier<?> valueSupplier = typeSuppliers.get(parameterType);
+        if (valueSupplier != null) {
+            // We do ! Go ahead an supply a value
+            return valueSupplier.get();
+        }
+
+        // We don't... Perhaps we can automatically reflect our way to provide it
+        Supplier<?> automaticTypeProvider = ValueProviders.factory(new ReflectiveTestFactory<>(parameterType));
+        typeSuppliers.put(parameterType, automaticTypeProvider);
+        return getValue(method);
+    }
+}
---
+++ src/test/java/be/witspirit/testfactory/support/ReflectiveTestFactoryTest.java
@@ -0,0 +1,58 @@
+package be.witspirit.testfactory.support;
+
+import be.witspirit.testfactory.exampledomain.Car;
+import be.witspirit.testfactory.exampledomain.EngineSpec;
+import be.witspirit.testfactory.exampledomain.User;
+import be.witspirit.testfactory.exampledomain.Vin;
+import be.witspirit.testfactory.factories.EngineSpecFactory;
+import be.witspirit.testfactory.factories.VinTestFactory;
+import be.witspirit.testfactory.support.valueproviders.ValueProviders;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Explore what is possible with the simple ReflectiveTestFactory
+ */
+public class ReflectiveTestFactoryTest {
+
+    @Test
+    public void reflectiveUser() {
+        ReflectiveTestFactory<User> factory = new ReflectiveTestFactory<>(User.class);
+
+        User user = factory.create();
+
+        Assert.assertNotNull(user);
+        Assert.assertTrue(user.getFirstName().length() >= 10 && user.getFirstName().length() <= 20);
+        Assert.assertTrue(user.getLastName().length() >= 10 && user.getLastName().length() <= 20);
+        Assert.assertTrue(user.getEmail().length() >= 10 && user.getEmail().length() <= 20);
+        Assert.assertTrue(user.getPhone().length() >= 10 && user.getPhone().length() <= 20);
+    }
+
+    @Test(expected = RuntimeException.class) // No default constructor
+    public void reflectiveVin() {
+        ReflectiveTestFactory<Vin> factory = new ReflectiveTestFactory<>(Vin.class);
+
+        factory.create();
+    }
+
+    @Test(expected = RuntimeException.class) // No default constructor
+    public void reflectiveEngineSpec() {
+        ReflectiveTestFactory<EngineSpec> factory = new ReflectiveTestFactory<>(EngineSpec.class);
+
+        factory.create();
+    }
+
+    @Test
+    public void reflectiveCar() {
+        ReflectiveTestFactory<Car> factory = new ReflectiveTestFactory<>(Car.class)
+                .provide(Vin.class, ValueProviders.factory(new VinTestFactory()))
+                .provide(EngineSpec.class, ValueProviders.factory(new EngineSpecFactory()));
+
+        Car car = factory.create();
+
+        Assert.assertEquals(17, car.getVin().toString().length()); // Horrible test, but not sure how to do better
+        Assert.assertNotNull(car.getEngine().getFuel());
+        Assert.assertTrue(car.getEngine().getDisplacement() >= 1500 && car.getEngine().getDisplacement() <= 2500);
+        Assert.assertTrue(car.getServiceComments().isEmpty());
+    }
+}
